6c721d4595ebf64eded0019b0451c716b5e3e742
rusgyn <rusgynmagsanay@gmail.com>
2024-10-29 14:16:30 -0400

    Create additional script that facilitate the input and output of the game. Separation of task.

---
+++ input_output_handler.rb
@@ -0,0 +1,26 @@
+# IO handler
+# Isolate your I/O logic to one or two classes. Don't allow all your classes to interact (gets/puts) with the User
+
+class IOHandler
+  #Input
+  def self.get_input(require)
+    print "#{require}"
+    gets.chomp
+
+  end
+
+  def self.display_message(message)
+    puts message
+  end
+
+  #Output
+  def self.display_lives(player1, player2)
+    puts "#{player1.name}: #{player1.lives}/3  vs  #{player2.name}: #{player2.lives}/3"
+  end
+
+  def self.winner(conqueror)
+    puts "#{conqueror.name} wins with a score of #{conqueror.lives}/3!"
+    puts "Game Over, Good bye!"
+  end
+end
+
